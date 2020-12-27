61af7351f935807d225a5f4e258d5581bdffd1b5
phoenixbyrd <james.gundlach79@gmail.com>
2020-12-27 11:30:37 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#Grab Updates
+git pull
+
+#Add new apps to menu
 cp /home/pi/webapps/DesktopFiles/* /home/pi/.local/share/applications/
+
+
 sudo apt install fortune lolcat cowsay telnet nyancat cmatrix -y
+
+#Install/Update finished
 clear
 
 echo ""
