82b546c0cadfa01c165ba58e6265eeb1e6ce3082
phoenixbyrd <james.gundlach79@gmail.com>
2020-12-27 11:27:26 -0600

    Redax Sounds

---
+++ redaxsounds.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --user-data-dir=/home/pi/.webapps/redaxsounds --app=https://redaxsounds.netlify.app/  --window-size=600,850
