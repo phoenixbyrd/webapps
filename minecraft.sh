010fd388dcd60cf42899303ec27c543f4aa4c91e
phoenixbyrd <james.gundlach79@gmail.com>
2020-12-27 14:39:29 -0600

    Minecraft Classic

---
+++ minecraft.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chromium-browser %U --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36" --user-data-dir=/home/pi/.webapps/minecraft --app=https://classic.minecraft.net/  --window-size=1024,768 
