6c11ea7c4fcf6f42b7822758207cfc47ef2b7461
phoenixbyrd <james.gundlach79@gmail.com>
2020-12-27 18:32:34 -0600

    fixed path in cowsay

--- TerminalDiversions/cowsay.sh
+++ TerminalDiversions/cowsay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xfce4-terminal --title=Cowsay --hide-menubar --hide-scrollbar -e '/home/pi/Documents/webapps/TerminalDiversions/cowsayit.sh'
+xfce4-terminal --title=Cowsay --hide-menubar --hide-scrollbar -e '/home/pi/webapps/TerminalDiversions/cowsayit.sh'
