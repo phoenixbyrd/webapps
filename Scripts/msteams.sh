49fe5be7144a0563781c87fc945dbf6a2464e7ce
phoenixbyrd <james.gundlach79@gmail.com>
2021-01-29 13:51:44 -0600

    .

---
+++ Scripts/msteams.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chromium-browser %U --user-data-dir=/home/pi/.config/webapps/MSTeams --app=https://teams.microsoft.com/  --window-size=600,800
---
+++ Scripts/treasurearena.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chromium-browser %U --user-data-dir=/home/pi/.config/webapps/treasurearena --app=http://titotu.io/embed/treasurearena-com  --window-size=1024,768 
