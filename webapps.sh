e95564707256fd968231ac50834725d22e4e8fa8
phoenixbyrd <james.gundlach79@gmail.com>
2021-01-29 13:28:02 -0600

    Revert "shortcut help"
    This reverts commit 13521bd17d274746562474d4b990ba49920ceb52.

--- webapps.sh
+++ webapps.sh
@@ -8,8 +8,5 @@ then
     rm -r /home/pi/.config/webapps/appData/Default/Cache
 fi
 
-chmod 755 /home/pi/WebApps/webapps/Scripts/*.sh
-
-
 #Web apps go brrrr
 chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --user-data-dir=/home/pi/.config/webapps/appData --app=file:///home/pi/WebApps/webapps/games.html 
